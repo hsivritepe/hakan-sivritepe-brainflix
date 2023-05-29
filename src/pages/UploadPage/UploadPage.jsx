3fa7a4504b80c5bcaaf88bf880d1635578dc511c
hsivritepe <hakan@sivritepe.com>
2023-05-28 21:24:20 -0700

    Change form to more React way with State

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -4,14 +4,34 @@ import uploadVideoImage from '../../assets/images/Upload-video-preview.jpg';
 import publishIcon from '../../assets/icons/publish.svg';
 
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function UploadPage(props) {
     const navigate = useNavigate();
 
+    // Handle form submission by interrupting and alerting the status
     const handleFormSubmit = (event) => {
         event.preventDefault();
         alert('Form submission is successful!');
         navigate('/');
+    };
+
+    const [videoTitle, setVideoTitle] = useState();
+    const [videoDescription, setVideoDescription] = useState();
+
+    const handleChangeVideoTitle = (event) => {
+        setVideoTitle(event.target.value);
+    };
+    const handleChangeVideoDescription = (event) => {
+        setVideoDescription(event.target.value);
+    };
+
+    // Validate title and description fields for input
+    const isFormValid = () => {
+        if (!videoTitle || !videoDescription) {
+            return false;
+        }
+        return true;
     };
 
     return (
@@ -19,7 +39,7 @@ function UploadPage(props) {
             {props.header}
             <section className="upload-page">
                 <form
-                    action=""
+                    action="/"
                     className="upload-page__form"
                     onSubmit={handleFormSubmit}
                 >
@@ -45,6 +65,8 @@ function UploadPage(props) {
                                 className="input-base"
                                 type="text"
                                 placeholder="Add a title to your video"
+                                name="videoTitle"
+                                onChange={handleChangeVideoTitle}
                             />
                             <p className="upload-page__subtitle">
                                 ADD A VIDEO DESCRIPTION
@@ -53,6 +75,10 @@ function UploadPage(props) {
                                 className="input-base upload-page__description"
                                 type="text"
                                 placeholder="Add a description to your video"
+                                name="videoDescription"
+                                onChange={
+                                    handleChangeVideoDescription
+                                }
                             />
                         </div>
                     </div>
@@ -60,6 +86,7 @@ function UploadPage(props) {
                         <button
                             type="submit"
                             className="button upload-page__button"
+                            disabled={!isFormValid()}
                         >
                             <img
                                 src={publishIcon}
@@ -69,12 +96,9 @@ function UploadPage(props) {
                             <span>PUBLISH</span>
                             <div className="button__button-spacer"></div>
                         </button>
-                        <a
-                            href=""
-                            className="button button-as-text upload-page__button"
-                        >
-                            <span>CANCEL</span>
-                        </a>
+                        <button className="button button-as-text upload-page__button">
+                            CANCEL
+                        </button>
                     </div>
                 </form>
             </section>
